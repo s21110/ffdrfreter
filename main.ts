143691145af395967aa29425506b5be380baaea1
s21110 <122764722+s21110@users.noreply.github.com>
2023-03-01 13:45:25 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,5 +4,8 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
     counter += 1
 })
+input.onButtonPressed(Button.B, function () {
+    counter = 0
+})
 let counter = 0
 counter = 0
